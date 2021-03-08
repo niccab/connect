155613b6b0d6d0d3cdf1299ad8517df993ad901f
Nicole Cabalquinto <35927237+niccab@users.noreply.github.com>
2021-03-07 20:18:41 -0500

    nedb2

--- week4/dbexample/server.js
+++ week4/dbexample/server.js
@@ -18,13 +18,21 @@ app.get("/", function (req, res) {
     res.send("Hello World!");
 });
 
+app.get('/displayrecord', function (req, res) {
+    db.find({ _id: req.query._id }, function (err, docs) {
+        var dataWrapper = { data: docs[0] };
+        res.render("individual.ejs", dataWrapper);
+    });
+});
+
 app.post('/formdata', function (req, res) {
     console.log(req.body.data);
 
     var dataToSave = {
         text: req.body.data,
         number: req.body.number,
-        color: req.body.color
+        color: req.body.color,
+        longtext: req.body.longtext
     };
 
     db.insert(dataToSave, function (err, newDoc) { 
